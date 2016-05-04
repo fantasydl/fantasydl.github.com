5143bd6496cfc360bb4d260550e2be8aa7b52526
FantasyDL <406215730@qq.com>
2016-05-04 16:14:58 +0800

    Update pulldrag.js

--- testmobile/js/pulldrag.js
+++ testmobile/js/pulldrag.js
@@ -481,7 +481,7 @@ PTR2.init = function(el, options) {
                     setTimeout(function(){
                         $(".scoll-bottom-fill").height(0);
                         resetTopAndBottom();
-                        $(".fixed").find("span").text('变0！！333');
+                        $(".fixed").find("span").text('');
                     },500)
                 }
             } else {
@@ -495,7 +495,7 @@ PTR2.init = function(el, options) {
       // cubic-bezier(.25,.66,.42,.99)
         PTR2.utils.transition(options.scrollTopElement, 'all', '500ms linear');
         PTR2.utils.transition(options.scrollBottomElement, 'all', '500ms linear');
-        if (!isAndroid && $(".scoll-bottom-fill").height() > 0) $(".scoll-bottom-fill").animate({'height':'0px'}, 400); //$(".fixed").find("span").text('变0！！333');
+        if (!isAndroid && $(".scoll-bottom-fill").height() > 0) $(".scoll-bottom-fill").animate({'height':'0px'}, 500); //$(".fixed").find("span").text('变0！！333');
         PTR2.utils.transform2(options.scrollTopElement, 0, 0);
         PTR2.utils.transform2(options.scrollBottomElement, 0, 0);
         if (options.scrollTopElement) options.scrollTopElement.style.opacity = 0;

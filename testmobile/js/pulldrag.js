83a8e97bc3bc4c0809161d1f3d3a95e649749ce1
FantasyDL <406215730@qq.com>
2016-05-04 16:19:05 +0800

    Update pulldrag.js

--- testmobile/js/pulldrag.js
+++ testmobile/js/pulldrag.js
@@ -480,10 +480,10 @@ PTR2.init = function(el, options) {
                 } else {
                     setTimeout(function(){
                         var firstH = $(".scoll-bottom-fill").height();
-                        $(".scoll-bottom-fill").height(0);
                         var secondH = $(".scoll-bottom-fill").height();
                         resetTopAndBottom();
-                        $(".fixed").find("span").text(firstH+''+secondH);
+                        $(".scoll-bottom-fill").height(0);
+                        $(".fixed").find("span").text(firstH+' '+secondH);
                     },500)
                 }
             } else {
@@ -497,7 +497,7 @@ PTR2.init = function(el, options) {
       // cubic-bezier(.25,.66,.42,.99)
         PTR2.utils.transition(options.scrollTopElement, 'all', '500ms linear');
         PTR2.utils.transition(options.scrollBottomElement, 'all', '500ms linear');
-        if (!isAndroid && $(".scoll-bottom-fill").height() > 0) $(".scoll-bottom-fill").animate({'height':'0px'}, 500); //$(".fixed").find("span").text('变0！！333');
+        if (!isAndroid && $(".scoll-bottom-fill").height() > 0) $(".scoll-bottom-fill").animate({'height':'0px'}, 400); //$(".fixed").find("span").text('变0！！333');
         PTR2.utils.transform2(options.scrollTopElement, 0, 0);
         PTR2.utils.transform2(options.scrollBottomElement, 0, 0);
         if (options.scrollTopElement) options.scrollTopElement.style.opacity = 0;

628a113ac3732e4100ec714df4734e3ea731fd03
FantasyDL <406215730@qq.com>
2016-05-05 10:28:33 +0800

    Update pulldrag.js

--- testmobile/js/pulldrag.js
+++ testmobile/js/pulldrag.js
@@ -448,7 +448,7 @@ PTR2.init = function(el, options) {
                         var targetTop = $(".scroll-container").get(0).scrollHeight - $(".scroll-container").get(0).offsetHeight;
                         var re = nowTop - targetTop;
                         if(re <= 55){
-                            $(".scoll-bottom-fill").height(re - (55 - re));
+                            $(".scoll-bottom-fill").height(re - 2*(55 - re));
                             $(".scroll-container").unbind('scroll');
                         }
                         $(".fixed").find("span").text('现在：' + re);
